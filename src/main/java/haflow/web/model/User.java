de26a509e382aad2b1b84c7f6bb90c79729f4e67
athenaer <liqiyuan312@163.com>
2013-05-02 10:22:46 +0800

    update

---
+++ src/main/java/haflow/web/model/User.java
@@ -0,0 +1,27 @@
+package haflow.web.model;
+
+public class User {
+	String name;
+	String password;
+	String sex;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+
+}
