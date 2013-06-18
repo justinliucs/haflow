1ef35d9667aadd15f3db2f22bf9c0f82d127c9e1
Zhen Tang <silenceofdaybreak@gmail.com>
2013-06-18 14:05:45 +0800

    Change the flow style.

--- src/test/java/haflow/test/NodeConfigurationServiceTest.java
+++ src/test/java/haflow/test/NodeConfigurationServiceTest.java
@@ -1,10 +1,14 @@
 package haflow.test;
 
+import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 @ContextConfiguration(locations = "classpath:servlet-context.xml")
 public class NodeConfigurationServiceTest extends
 		AbstractJUnit4SpringContextTests {
+	@Test
+	public void test() {
 
+	}
 }
