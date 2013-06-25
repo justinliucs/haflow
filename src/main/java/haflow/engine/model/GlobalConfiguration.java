b9f4b50f3ca1112849b01f335b012e3ceda1df46
zhaowei8188127 <18810460332@163.com>
2013-06-25 13:51:41 +0800

    update hive module.

--- src/main/java/haflow/engine/model/GlobalConfiguration.java
+++ src/main/java/haflow/engine/model/GlobalConfiguration.java
@@ -4,7 +4,6 @@ import haflow.utility.ClusterConfiguration;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,12 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class GlobalConfiguration {
 
-	private Properties properties;
-	
 	public static final String JOB_TRACKER = "job-tracker";
 	public static final String NAME_NODE = "name-node";
 	public static final String MAPRED_JOB_QUEUE_NAME = "mapred.job.queue.name";
 	public static final String OOZIE_USER_SYSTEM_LIBPATH = "oozie.use.system.libpath";
+	public static final String OOZIE_HIVE_CONNECTION_URL = "oozie.hive.connection.url";
+	public static final String OOZIE_HIVE_CONNECTION_NAME = "oozie.hive.connection.name";
+	public static final String OOZIE_HIVE_CONNECTION_PWD = "oozie.hive.connection.password";
+	
 	
 	private Map<String, String> map;
 	@Autowired
@@ -26,17 +27,11 @@ public class GlobalConfiguration {
 		map.put(JOB_TRACKER, cc.getProperty(JOB_TRACKER));
 		map.put(NAME_NODE, cc.getProperty(NAME_NODE));
 		map.put(MAPRED_JOB_QUEUE_NAME, cc.getProperty(MAPRED_JOB_QUEUE_NAME));
-		map.put(OOZIE_USER_SYSTEM_LIBPATH, cc.getProperty(OOZIE_USER_SYSTEM_LIBPATH));
+		map.put(OOZIE_USER_SYSTEM_LIBPATH, cc.getProperty(OOZIE_USER_SYSTEM_LIBPATH));		
 		
-		properties = new Properties();
-		properties.put(JOB_TRACKER, cc.getProperty(JOB_TRACKER));
-		properties.put(NAME_NODE, cc.getProperty(NAME_NODE));
-		properties.put(MAPRED_JOB_QUEUE_NAME, cc.getProperty(MAPRED_JOB_QUEUE_NAME));
-		properties.put(OOZIE_USER_SYSTEM_LIBPATH, cc.getProperty(OOZIE_USER_SYSTEM_LIBPATH));
-	}
-	
-	public String getConf(String key){
-		return this.properties.getProperty(key);
+		map.put(OOZIE_HIVE_CONNECTION_URL, cc.getProperty(OOZIE_HIVE_CONNECTION_URL));		
+		map.put(OOZIE_HIVE_CONNECTION_NAME, cc.getProperty(OOZIE_HIVE_CONNECTION_NAME));	
+		map.put(OOZIE_HIVE_CONNECTION_PWD, cc.getProperty(OOZIE_HIVE_CONNECTION_PWD));	
 	}
 	
 	public Map<String, String> getProperties(){
--- src/test/java/haflow/test/TestModule.java
+++ src/test/java/haflow/test/TestModule.java
@@ -1,11 +1,10 @@
 package haflow.test;
 
-import haflow.dto.entity.Node;
+import haflow.module.AbstractModule;
 import haflow.module.DataType;
 import haflow.module.Module;
 import haflow.module.ModuleConfiguration;
 import haflow.module.ModuleEndpoint;
-import haflow.module.AbstractModule;
 import haflow.module.ModuleType;
 
 import java.util.Map;
