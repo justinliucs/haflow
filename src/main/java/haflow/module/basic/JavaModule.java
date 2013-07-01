7c74a671c33078da3a7a33af6c12acc056892d21
Zhen Tang <silenceofdaybreak@gmail.com>
2013-07-01 11:57:10 +0800

    add regular expression validation to node configuration

--- src/main/java/haflow/module/basic/JavaModule.java
+++ src/main/java/haflow/module/basic/JavaModule.java
@@ -10,18 +10,18 @@ import haflow.module.ModuleType;
 import java.util.Map;
 
 @Module(id = "ada600a8-aa63-968a-ca46-9085e0e0bd2e", name = "Java", category = "Basic", type = ModuleType.JAVA, configurations = {
-		@ModuleConfiguration(key = "job-tracker", displayName = "Job Tracker", order = 1),
-		@ModuleConfiguration(key = "name-node", displayName = "Name Node", order = 2),
-		@ModuleConfiguration(key = "prepare.mkdir", displayName = "Prepare: Make Directory", order = 3),
-		@ModuleConfiguration(key = "prepare.delete", displayName = "Prepare: Delete", order = 4),
-		@ModuleConfiguration(key = "job-xml", displayName = "Job Xml", order = 5),
-		@ModuleConfiguration(key = "configuration", displayName = "Configuration", order = 6),
-		@ModuleConfiguration(key = "main-class", displayName = "Main Class", order = 7),
-		@ModuleConfiguration(key = "java-opts", displayName = "Java Options", order = 8),
-		@ModuleConfiguration(key = "arg", displayName = "Arguments", order = 9),
-		@ModuleConfiguration(key = "file", displayName = "File", order = 10),
-		@ModuleConfiguration(key = "archive", displayName = "Archive", order = 11),
-		@ModuleConfiguration(key = "capture-output", displayName = "Capture Output", order = 12) }, inputs = { @ModuleEndpoint(name = "from", minNumber = 1, maxNumber = 1, dataType = DataType.PlainText, order = 1) }, outputs = {
+		@ModuleConfiguration(key = "job-tracker", displayName = "Job Tracker", pattern = "^(.*)$", order = 1),
+		@ModuleConfiguration(key = "name-node", displayName = "Name Node", pattern = "^(.*)$", order = 2),
+		@ModuleConfiguration(key = "prepare.mkdir", displayName = "Prepare: Make Directory", pattern = "^(.*)$", order = 3),
+		@ModuleConfiguration(key = "prepare.delete", displayName = "Prepare: Delete", pattern = "^(.*)$", order = 4),
+		@ModuleConfiguration(key = "job-xml", displayName = "Job Xml", pattern = "^(.*)$", order = 5),
+		@ModuleConfiguration(key = "configuration", displayName = "Configuration", pattern = "^(.*)$", order = 6),
+		@ModuleConfiguration(key = "main-class", displayName = "Main Class", pattern = "^(.*)$", order = 7),
+		@ModuleConfiguration(key = "java-opts", displayName = "Java Options", pattern = "^(.*)$", order = 8),
+		@ModuleConfiguration(key = "arg", displayName = "Arguments", pattern = "^(.*)$", order = 9),
+		@ModuleConfiguration(key = "file", displayName = "File", pattern = "^(.*)$", order = 10),
+		@ModuleConfiguration(key = "archive", displayName = "Archive", pattern = "^(.*)$", order = 11),
+		@ModuleConfiguration(key = "capture-output", displayName = "Capture Output", pattern = "^(.*)$", order = 12) }, inputs = { @ModuleEndpoint(name = "from", minNumber = 1, maxNumber = 1, dataType = DataType.PlainText, order = 1) }, outputs = {
 		@ModuleEndpoint(name = "ok", minNumber = 1, maxNumber = 1, dataType = DataType.PlainText, order = 1),
 		@ModuleEndpoint(name = "error", minNumber = 1, maxNumber = 1, dataType = DataType.PlainText, order = 2) })
 public class JavaModule extends AbstractJavaModule {
