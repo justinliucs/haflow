2b03a896a041f993a8960f85aa495d15f0c4088c
zhaowei8188127 <18810460332@163.com>
2013-11-05 14:04:39 +0800

    add run history tab to the bottom.

--- src/main/java/haflow/module/zrace/HiveModule.java
+++ src/main/java/haflow/module/zrace/HiveModule.java
@@ -10,7 +10,7 @@ import haflow.module.ModuleType;
 
 import java.util.Map;
 
-@Module(id = "add600a8-aa63-8901-ca46-aaffa0e0bd2f", name = "Hive", category = "Basic", type = ModuleType.HIVE, configurations = {
+@Module(id = "add600a8-aa63-8901-ca46-aaffa0e0bd2f", name = "Hive", category = "DataSource", type = ModuleType.HIVE, configurations = {
 		@ModuleConfiguration(key = "sql", displayName = "Sql Command", pattern = "^(.*)$", type = ModuleConfigurationType.PLAIN_TEXT),
 		@ModuleConfiguration(key = "output_dir", displayName = "Output Directory", pattern = "^(.*)$", type = ModuleConfigurationType.PLAIN_TEXT), }, inputs = { @ModuleEndpoint(name = "from", minNumber = 1, maxNumber = 1, dataType = DataType.PlainText) }, outputs = {
 		@ModuleEndpoint(name = "ok", minNumber = 1, maxNumber = 1, dataType = DataType.PlainText),
